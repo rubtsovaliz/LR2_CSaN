0e27e627f0ff6134c4c327ba00eeeb5d57dbaafc
rubtsovaliz <l.rubtsoua@gmail.com>
2025-03-18 17:39:06 +0300

    Добавьте файлы проекта.

---
+++ ICMPPacket.cs
@@ -0,0 +1,66 @@
+using System;
+
+namespace LR2_CSaN
+{
+    class ICMPPacket
+    {
+        const int IP_HEADER_SIZE = 20;
+        const int ICMP_HEADER_SIZE = 4;
+        const int MESSAGE_MAX_SIZE = 1024;
+
+        private byte code;
+        private UInt16 checksum;
+        private int messageSize;
+        private byte[] message = new byte[MESSAGE_MAX_SIZE];
+
+        public byte Type { get; set; }
+
+        public int PacketSize { get; set; }
+
+        public ICMPPacket(byte type, byte[] data)
+        {
+            Type = type;
+            code = 0;
+            Buffer.BlockCopy(data, 0, message, ICMP_HEADER_SIZE, data.Length);
+            messageSize = data.Length + 4;  
+            PacketSize = messageSize + ICMP_HEADER_SIZE;
+            checksum = getChecksum();
+        }
+
+        public ICMPPacket(byte[] data, int size)
+        {
+            Type = data[IP_HEADER_SIZE];   
+            code = data[IP_HEADER_SIZE + 1];   
+            checksum = BitConverter.ToUInt16(data, IP_HEADER_SIZE + 2);  
+            PacketSize = size - IP_HEADER_SIZE;
+            messageSize = size - IP_HEADER_SIZE - ICMP_HEADER_SIZE;
+            Buffer.BlockCopy(data, IP_HEADER_SIZE + ICMP_HEADER_SIZE, message, 0, messageSize);
+        }
+
+        public byte[] getBytes()
+        {
+            byte[] data = new byte[PacketSize + 1];
+            data[0] = Type;
+            data[1] = code; 
+            Buffer.BlockCopy(BitConverter.GetBytes(checksum), 0, data, 2, 2);
+            Buffer.BlockCopy(message, 0, data, ICMP_HEADER_SIZE, messageSize);
+            return data;
+        }
+
+        public UInt16 getChecksum()
+        {
+            UInt32 checksum = 0;
+            byte[] data = getBytes();
+            int index = 0;
+
+            while (index < PacketSize)
+            {
+                checksum += Convert.ToUInt32(BitConverter.ToUInt16(data, index));
+                index += 2;
+            }
+            checksum = (checksum >> 16) + (checksum & 0xffff);
+            checksum += (checksum >> 16);
+            return (UInt16)(~checksum);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace LR2_CSaN
+{
+    class Program
+    {
+        const int TYPE_ECHO_REQUEST = 8;
+        const int MESSAGE_MAX_SIZE = 1024;
+
+        static void Main(string[] args)
+        {
+            byte[] message = new byte[MESSAGE_MAX_SIZE];
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.Icmp);
+            Console.Write("Введите IP-адрес для трассировки: ");
+            string ipAddress = Console.ReadLine(); 
+
+            try
+            {
+                IPEndPoint destIPEndPoint = new IPEndPoint(IPAddress.Parse(ipAddress), 0);
+                message = Encoding.ASCII.GetBytes("ICMP packet!");
+                ICMPPacket packet = new ICMPPacket(TYPE_ECHO_REQUEST, message);
+
+                Traceroute(socket, packet, destIPEndPoint);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Недопустимый IP-адрес: {0}.", ipAddress);
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Не удается связаться с узлом по адресу {0}.", ipAddress);
+            }
+            socket.Close();
+        }
+
+        static void Traceroute(Socket socket, ICMPPacket packet, IPEndPoint destIPEndPoint)
+        {
+            int timeStart, timeEnd, responseSize, errCount = 0;
+            byte[] responseMessage;
+
+            EndPoint hopEndPoint = destIPEndPoint;
+            socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 3000);
+            for (int i = 1; i < 50; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.IpTimeToLive, i);
+                    timeStart = Environment.TickCount;
+                    socket.SendTo(packet.getBytes(), packet.PacketSize, SocketFlags.None, destIPEndPoint);
+                    try
+                    {
+                        responseMessage = new byte[MESSAGE_MAX_SIZE];
+                        responseSize = socket.ReceiveFrom(responseMessage, ref hopEndPoint);
+                        timeEnd = Environment.TickCount;
+                        ICMPPacket response = new ICMPPacket(responseMessage, responseSize);
+
+                        Console.Write("{0} мс\t", timeEnd - timeStart);
+                        if (j == 2)
+                        {
+                            Console.WriteLine("{0}", hopEndPoint.ToString());
+                        }
+
+                        if ((response.Type == 0) && (j == 2))
+                        {
+                            Console.WriteLine("Трассировка завершена.");
+                            return;
+                        }
+                        errCount = 0;
+                    }
+                    catch (SocketException)
+                    {
+                        Console.Write("*\t");
+                        if (j == 2)
+                        {
+                            Console.WriteLine("{0}: Превышен интервал ожидания для запроса.", i);
+                        }
+                        errCount++;
+                        if (errCount == 30)
+                        {
+                            Console.WriteLine("Невозможно связаться с удаленным хостом.");
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
